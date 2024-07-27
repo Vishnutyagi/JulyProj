380c0003e8a672a0a4fa285de912f7d913d0e430
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-07-27 19:38:12 +0530

    adding component

---
+++ src/components/login.js
@@ -0,0 +1,92 @@
+import '../App.css';
+import React,{useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function Login() {
+  const [name,setName]=useState("");
+  const [password,setPassword]=useState("");
+  const navigate = useNavigate(); 
+
+  const [error,setError]=useState("");
+
+  const handlename=(e)=>{
+    setName(e.target.value);
+  }
+  const handlepassword=(e)=>{
+    setPassword(e.target.value);
+  }
+  const handleClick = async (e) => {
+    e.preventDefault(); // Prevent form from submitting normally
+
+    try {
+      const response = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: name,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('user', JSON.stringify(data));
+        navigate('/profile');
+      } else {
+        setError(data.message || "Login failed. Please try again.");
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      setError('An error occurred. Please try again.');
+    }
+  };
+
+  return (
+    <div className="main">
+      <div className="dotted">
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+      </div>
+      <div className="content">
+        <div className="card-container">
+          <div className="card">
+            <p>Welcome back! 👋</p>
+            <h2>Sign in to your account</h2>
+            <form>
+              <label for="email">Your email</label>
+              <input id="email" type="text" onChange={handlename}></input>
+              <label for="pass">Password</label>
+              <input id="pass" type="text" onChange={handlepassword}></input>
+              <button type="submit" className="continue" onClick={handleClick}>CONTINUE</button>
+            </form>
+            <div style={{"text-align":"center"}}>
+            <button className="signup">Forget your password?</button>
+            </div>
+          </div>
+          <div style={{"text-align":"center",margin:"20px"}}>
+            <p>Don’t have an account? <button className="signup">Sign up</button></p>
+            {error&&<p style={{color:"red"}}>{error}</p>}
+          </div>
+        </div>
+          
+      </div>
+      <div className="white">
+        <hr className="line1" style={{height:"5px",width:"200px"}}></hr>
+        <hr className="line2" style={{height:"5px",width:"200px"}}></hr>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/profile.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+      console.log(storedUser)
+      if (storedUser && storedUser.id) {
+        try {
+          console.log("fsdf")
+          const response = await fetch(`https://dummyjson.com/users/${storedUser.id}`);
+          const data = await response.json();
+          localStorage.setItem('profile', JSON.stringify(data));
+          console.log(data)
+          setUser(data);
+        } catch (error) {
+          console.error('Error fetching profile:', error);
+        }
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="dotted">
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+        <hr className="lines"></hr>
+      </div>
+      <div className="content">
+        <div className="card-container">
+          <div className="card">
+          {user ? (
+        <>
+          <h2>{user.username}'s Profile</h2>
+          <p>Id: {user.id}</p>
+          <p>Name: {user.firstName} {user.lastName}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Gender: {user.gender}</p>
+          <p>Image: {<img src={user.image} alt="image"></img>}</p>
+        </>
+      ) : (
+        <p>Loading profile...</p>
+      )}
+            
+          </div>
+    
+        </div>
+          
+      </div>
+      <div className="white">
+        <hr className="line1" style={{height:"5px",width:"200px"}}></hr>
+        <hr className="line2" style={{height:"5px",width:"200px"}}></hr>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
